3e4ff827f9983ad6b189232754261115fa63b4b8
harini-coder <hariniultimate56@gmail.com>
2020-01-11 12:06:12 +0530

    design

---
+++ src/for_github1/Gmail.java
@@ -0,0 +1,16 @@
+package for_github1;
+
+public class Gmail {
+
+	public static void main(String[] args) {
+		String s="hi";
+		String s1=new String("hi");
+		boolean b = s.equalsIgnoreCase(s1);
+		boolean b1 = s==s1;
+		System.out.println(b);
+		System.out.println(b1 );
+		System.out.println("Hi");
+
+	}
+
+}
